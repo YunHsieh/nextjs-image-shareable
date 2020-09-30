f030d6d54cf6bbf3d8e2a0e162c1713617a31bdf
JerryHsieh <echoyun.hsieh@gmail.com>
2020-10-01 01:53:14 +0800

    add: auto Scroll

--- components/containers/home/index.js
+++ components/containers/home/index.js
@@ -1,5 +1,4 @@
 import React, { createRef } from 'react';
-
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchTestApi } from '../../../store/modules/get_api'
@@ -10,16 +9,61 @@ class Test extends React.PureComponent {
     
     constructor(props) {
         super(props);
-        this.myRef = createRef([]);
-        this.myRef.current = []
+        this.rightRef = createRef([]);
+        this.rightRef.current = [];
+        this.leftRef = createRef([]);
+        this.leftRef.current = [];
         this.leftElement = [];
         this.rightElement = [];
+
+        this.rightCurrentidx = 0;
+        this.leftCurrentidx = 0;
     }
 
     onMouseMove(e) {
         console.log('move in here');
         this.moving && this.onMove(e);
     }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll, { passive: true })
+      }
+    
+      componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+      }
+    
+      listenScrollEvent(event) {
+          let currentScrollPosition = this.refs.leftScreen.scrollTop;
+
+          let rightLastScrollTop = this.rightRef.current[0 + this.rightCurrentidx].offsetTop;
+          let rightNextScrollTop = this.rightRef.current[2 + this.rightCurrentidx].offsetTop;
+
+          let leftLastScrollTop = this.leftRef.current[0 + this.leftCurrentidx].offsetTop;
+          let leftNextScrollTop = this.leftRef.current[1 + this.leftCurrentidx].offsetTop;
+
+          if (leftLastScrollTop > currentScrollPosition){
+            this.rightCurrentidx-=2;
+            this.leftCurrentidx-=1;
+          } else if (leftNextScrollTop < currentScrollPosition){
+            this.rightCurrentidx+=2;
+            this.leftCurrentidx+=1;
+          }
+
+          if (this.rightCurrentidx < 0 || this.leftCurrentidx < 0){
+            this.rightCurrentidx = 0;
+            this.leftCurrentidx = 0;
+          }
+
+          if (this.leftCurrentidx >= this.leftRef.current.length - 1){
+            this.rightCurrentidx = this.rightRef.current.length - 4;
+            this.leftCurrentidx = this.leftRef.current.length - 2;
+          }
+          let rightRatePercent = 1 - (leftNextScrollTop - currentScrollPosition) / (leftNextScrollTop - leftLastScrollTop)
+          let leftRatePosition = rightLastScrollTop + ((rightNextScrollTop - rightLastScrollTop) * rightRatePercent)
+
+          window.scrollTo({  top: leftRatePosition })
+      }
     
     render(){
         const {
@@ -27,7 +71,7 @@ class Test extends React.PureComponent {
         } = this.props;
 
         const executeScroll = (index) => {
-            window.scrollTo({ behavior: 'smooth', top: this.myRef.current[index].offsetTop })
+            window.scrollTo({ behavior: 'smooth', top: this.rightRef.current[index].offsetTop })
         };
         // sort the data display image in album and image detail
         payload.forEach((data, i) => {
@@ -35,17 +79,20 @@ class Test extends React.PureComponent {
                 const idx = (i * data['data'].length) + j;
                 if (j === 0){
                     this.leftElement.push(
-                        <ImgControl onClick={executeScroll.bind(this, idx)} 
+                        <ImgControl onClick={executeScroll.bind(this, idx)}
+                            ref={el => {
+                                this.leftRef.current[i] = el;
+                            }}
                             onMouseMove={e => this.onMouseMove(e)} 
                             key={idx} 
                             src={subData['image_url']}/>
-                        );
+                    );
                 }
                 
                 this.rightElement.push(<ImgControl 
                     ref={el => {
-                        this.myRef.current[idx] = el;
-                    }} 
+                        this.rightRef.current[idx] = el;
+                    }}
                     onMouseMove={e => this.onMouseMove(e)} 
                     key={`img-${idx}`} 
                     src={subData['image_url']}/>
@@ -56,7 +103,7 @@ class Test extends React.PureComponent {
         return(
             <>  
                 <div className="container">
-                    <HalfLeft>
+                    <HalfLeft ref="leftScreen" onScroll={this.listenScrollEvent.bind(this)}>
                         {this.leftElement}
                     </HalfLeft>
                     <HalfRight>
